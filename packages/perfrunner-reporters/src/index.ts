2d0f67261acd851ebe5f7100bdd9b3dc9c6d263e
Vitalii Drag <ruban.vitaliy@gmail.com>
2020-06-14 12:56:49 +0300

    Added reporter/index.ts under src folder

---
+++ packages/perfrunner-reporters/src/index.ts
@@ -0,0 +1,11 @@
+import { IReporter } from "./reporters/iReporter";
+import { toJson } from "./reporters/json";
+import { toCsv } from "./reporters/csv";
+import { defaultReporter as basic } from "./reporters/default";
+
+export {
+    IReporter,
+    toJson,
+    toCsv,
+    basic,
+};
