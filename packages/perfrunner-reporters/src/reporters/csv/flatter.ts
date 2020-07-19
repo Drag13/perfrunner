922dbd908f6d6cacdbd32b0b321501be11aed27a
Vitalii Drag <ruban.vitaliy@gmail.com>
2020-07-19 17:36:43 +0300

    Reworked CSV reporter

---
+++ packages/perfrunner-reporters/src/reporters/csv/flatter.ts
@@ -0,0 +1,17 @@
+import { IPerformanceResult } from 'perfrunner-core';
+import { perfEntriesToCsv } from './perf-entry-mapper';
+import { metricsToCsv } from './metrics-mapper';
+import { FlattenPerformanceResult } from './flatten-performance-result';
+
+function flatten(rawData: IPerformanceResult) {
+    const result: FlattenPerformanceResult[] = [];
+
+    return rawData.reduce((acc, testResult, id) => {
+        const entries = perfEntriesToCsv(testResult);
+        const metrics = metricsToCsv(testResult, id);
+
+        return acc.concat(entries, metrics);
+    }, result);
+}
+
+export { flatten };
