5852fc6773e14c85bbce5a35d58190cffaa34644
Vitalii Drag <ruban.vitaliy@gmail.com>
2020-07-20 18:03:58 +0300

    Added executable path

--- packages/perfrunner-core/src/profiler/perf-options.ts
+++ packages/perfrunner-core/src/profiler/perf-options.ts
@@ -37,12 +37,7 @@ export interface PerfOptions {
     useCache?: boolean;
 }
 
-export interface RunnerOptions {
-    /**
-     * Sets number of runs you want to execute
-     */
-    runs: number;
-
+export interface ILunchOptions {
     /**
      * Sets timeout for the test
      */
@@ -54,11 +49,6 @@ export interface RunnerOptions {
     headless?: boolean;
 
     /**
-     * Sets time (in miliseconds) or selector you want to wait for
-     */
-    waitFor?: number | string;
-
-    /**
      * Sets list of chrome arguments
      */
     chromeArgs?: string[];
@@ -67,9 +57,24 @@ export interface RunnerOptions {
      * Sets flag to ignore default arguments
      */
     ignoreDefaultArgs?: boolean;
+
+    /**
+     * Sets path to the Chrome instance
+     */
+    executablePath?: string;
 }
 
-export interface PerfRunnerOptions extends PerfOptions, RunnerOptions {
+export interface RunnerOptions {
+    /**
+     * Sets number of runs you want to execute
+     */
+    runs: number;
+
+    /**
+     * Sets time (in miliseconds) or selector you want to wait for
+     */
+    waitFor?: number | string;
+
     /**
      * Sets folder to store data
      */
@@ -95,3 +100,5 @@ export interface PerfRunnerOptions extends PerfOptions, RunnerOptions {
      */
     reportOnly?: boolean;
 }
+
+export interface PerfRunnerOptions extends PerfOptions, RunnerOptions, ILunchOptions {}
